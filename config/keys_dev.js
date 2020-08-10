849e5774f254b5bd78447e2579aa2d28ba21ec6f
Wilson Duan <wilsonduan@Wilsons-MacBook-Pro.local>
2020-08-10 15:46:34 -0700

    asasasa

--- config/keys_dev.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    stripeSecretKey: 'sk_test_51H8aS2IZmt4LvphqT1qFzIo7K4OvLFPRQhfJvQEp4GrnutT3eZr57Ss6xLoazOgIgnwGrvVX1Kth4renTFmMp1lN008iCw4ewf',
-    mongoURI: "mongodb+srv://Wilson:hightide307@cluster0-k8dwp.mongodb.net/Cluster0?retryWrites=true&w=majority",
-    jwtSecret: "myjwtSecret",
-    jwtSecret2: "emailSecret"
-}
