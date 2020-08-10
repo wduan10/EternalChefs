3516ce909dd349a3c3c9127a8d20976b7867b316
Wilson Duan <wilsonduan@Wilsons-MacBook-Pro.local>
2020-08-10 15:41:08 -0700

    ok

---
+++ config/keys_prod.js
@@ -0,0 +1,6 @@
+module.exports = {
+    stripeSecretKey: process.env.STRIPE_SECRET_KEY,
+    mongoURI: process.env.MONGO_URI,
+    jwtSecret: process.env.JWT_SECRET,
+    jwtSecret2: process.env.JWT_SECRET2
+}
