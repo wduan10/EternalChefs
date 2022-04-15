45a7f1d2bdad1a778f8122c74f47c83b7741127d
Wilson Duan <wilsonduan@Wilsons-MacBook-Pro.local>
2022-04-15 14:50:58 -0700

    update

---
+++ client/build/precache-manifest.4beaea89840a5dee3919b5b10dca9a5f.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f3041f61981c639973e17d274abd53c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b5d7c3a18a9c99aae75",
+    "url": "/static/css/2.5544c887.chunk.css"
+  },
+  {
+    "revision": "906649851d6cbc54b4fe",
+    "url": "/static/css/main.7958b078.chunk.css"
+  },
+  {
+    "revision": "4b5d7c3a18a9c99aae75",
+    "url": "/static/js/2.c2cb7c81.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c2cb7c81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "906649851d6cbc54b4fe",
+    "url": "/static/js/main.01b3fe82.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "0b620655e16e3d208749f6f243355b0a",
+    "url": "/static/media/Instagram.0b620655.png"
+  },
+  {
+    "revision": "1ddd72b5861c2f486a856dd0ea7f74ec",
+    "url": "/static/media/Success.1ddd72b5.JPG"
+  },
+  {
+    "revision": "fdf448aa74a7c23344d105303fe11e6b",
+    "url": "/static/media/Twitter.fdf448aa.png"
+  },
+  {
+    "revision": "839d125bb973735f908e66e14d3550ae",
+    "url": "/static/media/img1.839d125b.JPG"
+  },
+  {
+    "revision": "f8bf19292f1144ce76300f0679d7627d",
+    "url": "/static/media/logo2.f8bf1929.png"
+  },
+  {
+    "revision": "7ad95bbe6a051558fe89f2764e718b29",
+    "url": "/static/media/purchase.7ad95bbe.JPG"
+  }
+]);
